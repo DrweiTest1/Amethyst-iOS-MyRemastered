00de5d942121fcdf01e85c52a9a37a55d7452af8
Drwei <111724449+herbrine8403@users.noreply.github.com>
2025-08-21 02:52:11 +0800

    Support third-party (Yggdrasil-style) auth: BaseAuthenticator, UI and CMake changes

--- Natives/authenticator/BaseAuthenticator.m
+++ Natives/authenticator/BaseAuthenticator.m
@@ -20,7 +20,7 @@ static BaseAuthenticator *current = nil;
 }
 
 + (id)loadSavedName:(NSString *)name {
-    NSMutableDictionary *authData = parseJSONFromFile([NSString stringWithFormat:@"%s/accounts/%@.json", getenv("POJAV_HOME"), name]);
+    NSMutableDictionary *authData = parseJSONFromFile([NSString stringWithFormat:"%s/accounts/%@.json", getenv("POJAV_HOME"), name]);
     if (authData[@"NSErrorObject"] != nil) {
         NSError *error = ((NSError *)authData[@"NSErrorObject"]);
         if (error.code != NSFileReadNoSuchFileError) {
@@ -29,10 +29,30 @@ static BaseAuthenticator *current = nil;
         return nil;
     }
 
-    if ([authData[@"expiresAt"] longValue] == 0) {
-        return [[LocalAuthenticator alloc] initWithData:authData];
-    } else { 
-        return [[MicrosoftAuthenticator alloc] initWithData:authData];
+    // If authData explicitly tells type, honor it. Otherwise, fall back to legacy expiresAt heuristic.
+    NSString *type = authData[@"type"];
+    if (type != nil) {
+        if ([type isEqualToString:@"local"]) {
+            return [[LocalAuthenticator alloc] initWithData:authData];
+        } else if ([type isEqualToString:@"thirdparty"]) {
+            Class cls = NSClassFromString(@"ThirdPartyAuthenticator");
+            if (cls) {
+                return [[cls alloc] initWithData:authData];
+            } else {
+                // fallback
+                return [[MicrosoftAuthenticator alloc] initWithData:authData];
+            }
+        } else {
+            // default to MicrosoftAuthenticator for other online types
+            return [[MicrosoftAuthenticator alloc] initWithData:authData];
+        }
+    } else {
+        // legacy: expiresAt == 0 -> local; else Microsoft
+        if ([authData[@"expiresAt"] longValue] == 0) {
+            return [[LocalAuthenticator alloc] initWithData:authData];
+        } else {
+            return [[MicrosoftAuthenticator alloc] initWithData:authData];
+        }
     }
 }
 
@@ -59,9 +79,9 @@ static BaseAuthenticator *current = nil;
 
     [self.authData removeObjectForKey:@"input"];
 
-    NSString *newPath = [NSString stringWithFormat:@"%s/accounts/%@.json", getenv("POJAV_HOME"), self.authData[@"username"]];
+    NSString *newPath = [NSString stringWithFormat:"%s/accounts/%@.json", getenv("POJAV_HOME"), self.authData[@"username"]];
     if (self.authData[@"oldusername"] != nil && ![self.authData[@"username"] isEqualToString:self.authData[@"oldusername"]]) {
-        NSString *oldPath = [NSString stringWithFormat:@"%s/accounts/%@.json", getenv("POJAV_HOME"), self.authData[@"oldusername"]];
+        NSString *oldPath = [NSString stringWithFormat:"%s/accounts/%@.json", getenv("POJAV_HOME"), self.authData[@"oldusername"]];
         [NSFileManager.defaultManager moveItemAtPath:oldPath toPath:newPath error:&error];
         // handle error?
     }
